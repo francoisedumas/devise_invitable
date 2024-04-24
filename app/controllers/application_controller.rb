ef689d813217f71815a77c5021120ad88d2a7a6c
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2024-04-24 17:46:17 +0200

    Setup after_setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  def active_menu_link
+    root_path
+  end
+  helper_method :active_menu_link
 end
